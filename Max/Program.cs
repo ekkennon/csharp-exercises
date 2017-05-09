4213b5620529c2519c09e5a6c9e0a490e902c6fc
Erin K. Kennon <ekkennon@gmail.com>
2017-05-09 15:34:49 -0500

    finished walkthrough day1

---
+++ Max/Program.cs
@@ -0,0 +1,39 @@
+﻿//walkthrough day 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lst = new int [10];
+
+            for (int i=0; i<10; i++)
+            {
+                lst[i] = i;
+            }
+
+            Console.WriteLine(FindMax(lst));
+        }
+
+        static int FindMax(int[] lst)
+        {
+            int max = lst[0];
+
+            foreach (int i in lst)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+
+            return max;
+        }
+    }
+}
