19a63bce10de8b5cac541bb06910a8fbae471766
Erin K. Kennon <ekkennon@gmail.com>
2017-05-09 15:41:29 -0500

    finished studio day 1

---
+++ CircleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿//studio day 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius");
+            Console.WriteLine(CalcArea(double.Parse(Console.ReadLine())));
+        }
+
+        static double CalcArea(double r)
+        {
+            return r * r * Math.PI;
+        }
+    }
+}
