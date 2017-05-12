30ec3475db8c02a7debc1f7ca8440f94d56073d6
Erin K. Kennon <ekkennon@gmail.com>
2017-05-11 19:31:24 -0500

    finished studio

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,34 @@
+﻿//studio day 2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "u";
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (d.ContainsKey(c))
+                {
+                    d[c] = d[c] + 1;
+                } else
+                {
+                    d.Add(c, 1);
+                }
+            }
+            foreach (char count in d.Keys)
+            {
+                Console.WriteLine(count + ": " + d[count]);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post'> <input type='text' name='name' /> <input type='submit' value='Greet me!' /></form>";
+
+            return Content(html, "text/html");
+        }
+
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        //Handle requests to /Hello/NAME (URL segment)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        // need to comment out above method for this to work
+        public IActionResult Holder1(string name)
+        {
+            return Redirect("/Hello/Aloha");
+        }
+
+    // alter the route to the controller to be: /Hello/Aloha
+    [Route("/Hello/Aloha")]
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+}
