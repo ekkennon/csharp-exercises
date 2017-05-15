85b42ea3630d72d4fee508e02dddc7eaae01c543
Erin K. Kennon <ekkennon@gmail.com>
2017-05-15 11:35:18 -0500

    finished prep work day 4

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,42 @@
+﻿//day 4 prep
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        private static Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string desc)
+        {
+            Cheeses.Add(name, desc);
+            return Redirect("/Cheese");
+        }
+    }
+}
