618f7d96e79ddbbd97e5f66d857eceb52e066e04
Erin K. Kennon <ekkennon@gmail.com>
2017-05-19 19:01:06 -0500

    finished prep work 6

--- School/Course.cs
+++ School/Course.cs
@@ -16,5 +16,39 @@ namespace School
         public List<string> prereqs { get; set; }
         public List<Student> Students { get; set; }
 
+        public Course (string id, string name, string desc, string code, int credits)
+        {
+            CourseID = id;
+            CourseName = name;
+            CourseDesc = desc;
+            OfferCode = code;
+            NumCredits = credits;
+        }
+
+        public Course(string id, string code) : this(id, "", "", code, 0)
+        {
+
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Course c = obj as Course;
+
+            if (CourseID.Equals(c.CourseID))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return CourseID +  " - " + CourseName;
+        }
     }
 }
--- School/Student.cs
+++ School/Student.cs
@@ -6,25 +6,108 @@ using System.Threading.Tasks;
 
 namespace School
 {
-    class Student
-    {
-        //todo create constructor
+    class Student { 
 
+        private static int nextStudentId = 1;
         public int NumCredits { get; set; }
         public double GPA { get; set; }
+        public string Name { get; set; }
         private int studentID;
-        private string name;
+        private double qualityScore;
+
+        public Student(string name, int studentId, int numCredits, double gpa, double qs)
+        {
+            NumCredits = numCredits;
+            GPA = gpa;
+            Name = name;
+            StudentID = studentId;
+            QualityScore = qs;
+        }
+
+        public Student(string name, int studentId) : this(name, studentId, 0, 0, 0)
+        {
+
+        }
+
+        public Student(string name) : this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
 
         public int StudentID
         {
             get { return studentID; }
-            private set { studentID = value; }// this should only be set from the constructor
+            private set { studentID = value; }
         }
 
-        public string MyProperty
+        public double QualityScore
         {
-            get { return name; }
-            set { name = value; }
+            get { return qualityScore; }
+            private set { qualityScore = value; }
+        }
+
+        public string GetGradeLevel()
+        {
+            if (NumCredits < 30)
+            {
+                return "Freshman";
+            } else if (NumCredits < 60)
+            {
+                return "Sophomore";
+            } else if (NumCredits < 90)
+            {
+                return "Junior";
+            } else
+            {
+                return "Senior";
+            }
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            NumCredits += courseCredits;
+
+            double qs = 0;
+            if (grade > 89.4)
+            {
+                qs = 4;
+            } else if (grade > 79.4)
+            {
+                qs = 3;
+            } else if (grade > 69.4)
+            {
+                qs = 2;
+            } else if (grade > 59.4)
+            {
+                qs = 1;
+            } else
+            {
+                qs = 0;
+            }
+            QualityScore += (qs * courseCredits);
+
+            GPA = QualityScore / NumCredits;
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Student s = obj as Student;
+
+            if (StudentID.Equals(s.StudentID))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return StudentID + " " + Name + " - " + GPA;
         }
     }
 }
