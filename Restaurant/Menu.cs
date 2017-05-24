0dc9e104807e70a7f21ef8d903ee52d4190a23d9
Erin K. Kennon <ekkennon@gmail.com>
2017-05-24 15:32:14 -0500

    finished studio 5

---
+++ Restaurant/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant
+{
+    class Menu
+    {
+        public List<MenuItem> Items { get; set; }
+        public DateTime Updated { get; set; }
+        public string Type { get; set; }//breakfast,lunch,dinner
+
+        public Menu(List<MenuItem> items, DateTime updated, string type)
+        {
+            Items = items;
+            Updated = updated;
+            Type = type;
+        }
+
+        public Menu(string type) : this(new List<MenuItem>(), DateTime.Now, type)
+        {
+        }
+    }
+}
---
+++ Restaurant/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Restaurant
+{
+    class MenuItem
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Category { get; set; }//appetizer, dessert, main course
+        public bool IsNew { get; set; }
+
+        public MenuItem(string name, string desc, string cat, double price)
+        {
+            Price = price;
+            Name = name;
+            Desc = desc;
+            Category = cat;
+            IsNew = true;
+        }
+    }
+}
